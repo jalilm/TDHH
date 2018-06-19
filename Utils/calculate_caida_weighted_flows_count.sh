1b87f6cfbd2c67c88184c061f561608afac887f5
Jalil Moraney <jalilm@cs.technion.ac.il>
2018-06-19 21:19:02 +0300

    Added Utils documentation.

--- Utils/calculate_caida_weighted_flows_count.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cut -d"," -f3,4,5 ../files/pkts.csv | sed -E 's/length:(.*),IP_SRC:(.*),IP_DST:(.*)/\1 \2:\3/' | awk '{a[$2] += $1} END {for (i in a) { printf "%s %s\n", a[i] ,i | "sort -rn";}}' > ../files/pkts_weighted_flows_count.csv
-awk '{s+=$1}END{print s}' ../files/pkts_weighted_flows_count.csv > ./pkts_total_weight
-cat ./pkts_total_weight ../files/pkts_weighted_flows_count.csv > ./tmp
-rm ./pkts_total_weight
-mv ./tmp ../files/pkts_weighted_flows_count.csv
-
